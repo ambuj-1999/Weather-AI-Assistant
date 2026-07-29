bcda18d986fd5a6db74d5ccfcdd4b0711b486f31
b31user31 <b31user31@rpsconsulting.in>
2026-07-29 13:07:22 +0530

    Prepare for render deployment

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.post("/ask-ai", async (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port ", PORT);
 });
